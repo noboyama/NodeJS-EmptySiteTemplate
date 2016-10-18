08b7befc8e3314cb974892baace82318ad9ff2eb
noboyama <noboyama@yk.rim.or.jp>
2016-10-18 17:53:29 +0900

    aaaa

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    
+
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
+    res.end('Hello, aaa world!');
+
 }).listen(process.env.PORT || 8080);
